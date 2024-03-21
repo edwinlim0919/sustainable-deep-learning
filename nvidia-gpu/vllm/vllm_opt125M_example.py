9e7b4ed94ea4f6f37c04e7e33a281a2890a17ddb
edwinlim <edwinlim@node0.v10032gb.datacenter-soc-pg0.clemson.cloudlab.us>
2024-03-21 17:35:50 -0400

    vllm examples

--- nvidia-gpu/vllm/vllm_opt125M_example.py
+++ nvidia-gpu/vllm/vllm_opt125M_example.py
@@ -10,3 +10,11 @@ prompts = [
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 llm = LLM(model="facebook/opt-125m")
+
+outputs = llm.generate(prompts, sampling_params)
+
+# Print the outputs.
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
