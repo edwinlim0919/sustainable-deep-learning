db643c13896c96e98c3ad62421f09f4aba96f7a6
edwinlim <edwinlim@node0.v10032gb.datacenter-soc-pg0.clemson.cloudlab.us>
2024-03-25 10:13:14 -0400

    chkpt

--- nvidia-gpu/vllm/test_vllm_llama2_local.py
+++ nvidia-gpu/vllm/test_vllm_llama2_local.py
@@ -10,14 +10,32 @@ if __name__ == '__main__':
         '--test-prompt',
         type=str,
         required=True,
-        help='test prompt for async engine'
+        help='test llama2 prompt for async vllm engine'
     )
-    #parser = AsyncEngineArgs.add_cli_args(parser)
+    parser.add_argument(
+        '--test-output',
+        type=str,
+        required=True,
+        help='test llama2 output for async vllm engine'
+    )
     parser = vllm_llama2_local.add_cli_args_wrapper(parser)
     args = parser.parse_args()
-
     vllm_llama2_local.vllm_setup(args)
-    asyncio.run(vllm_llama2_local.generate(
+
+    output_token_ids = vllm_llama2_local.tokenizer(
+        args.test_output,
+        return_tensors='pt'
+    ).input_ids[0]
+    num_output_tokens = len(output_token_ids)
+
+    print(f'test_prompt: {args.test_prompt}')
+    print(f'test_output: {args.test_output}')
+    print(f'output_token_ids: {output_token_ids}')
+    print(f'num_output_tokens: {num_output_tokens}')
+    final_output, request_latency = asyncio.run(vllm_llama2_local.generate(
         args.test_prompt,
+        num_output_tokens,
         '0'
     ))
+    print(f'final_output: {final_output}')
+    print(f'request_latency: {request_latency}')
--- nvidia-gpu/vllm/vllm_llama2_local.py
+++ nvidia-gpu/vllm/vllm_llama2_local.py
@@ -5,9 +5,11 @@ from vllm.sampling_params import SamplingParams
 from vllm.utils import random_uuid
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.engine.arg_utils import AsyncEngineArgs
+from transformers import AutoTokenizer
 
 
 vllm_engine = None
+tokenizer = None
 
 
 def add_cli_args_wrapper(parser):
@@ -16,28 +18,44 @@ def add_cli_args_wrapper(parser):
 
 def vllm_setup(args):
     global vllm_engine
+    global tokenizer
+
     engine_args = AsyncEngineArgs.from_cli_args(args)
     vllm_engine = AsyncLLMEngine.from_engine_args(engine_args)
+    tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-chat-hf')
 
 
 async def generate(
     test_prompt,
+    num_output_tokens,
     request_id
 ):
-    sampling_params = SamplingParams()
+    request_start_time = time.time()
+    prompt_token_ids = tokenizer(
+        test_prompt,
+        return_tensors='pt'
+    ).input_ids
+    prompt_token_ids_list = prompt_token_ids.tolist()[0]
+    print(f'GENERATE DEBUG type(prompt_token_ids): {type(prompt_token_ids)}')
+    print(f'prompt_token_ids: {prompt_token_ids}')
+    print(f'prompt_token_ids_list: {prompt_token_ids_list}')
+    num_input_tokens = len(prompt_token_ids)
+    max_tokens = min(2000 - num_input_tokens, num_output_tokens)
+    sampling_params = SamplingParams(
+        max_tokens=max_tokens
+    )
 
-    request_enqueue_time = time.time()
     results_generator = vllm_engine.generate(
         test_prompt,
         sampling_params,
-        request_id
+        request_id,
+        prompt_token_ids_list
     )
 
     final_output = None
     async for request_output in results_generator:
         final_output = request_output
-    request_dequeue_time = time.time()
+    request_end_time = time.time()
 
-    request_latency = request_dequeue_time - request_enqueue_time
-    print(f'final_output: {final_output}')
-    print(f'request_latency: {request_latency}')
+    request_latency = request_end_time - request_start_time
+    return final_output, request_latency
