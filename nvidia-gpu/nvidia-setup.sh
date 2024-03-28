b985f86c69d348b447995175ae31643fc2a0d643
edwinlim <edwinlim@node0.a100.datacenter-soc-pg0.wisc.cloudlab.us>
2024-03-28 11:35:42 -0500

    rdme

--- nvidia-gpu/nvidia-setup.sh
+++ nvidia-gpu/nvidia-setup.sh
@@ -4,8 +4,10 @@
 #   1: link to CUDA installer from https://developer.nvidia.com/cuda-12-1-0-download-archive
 #   2: filename of .run installer
 #
-# Example Usage
+# Example Usage (CUDA 12.1)
 #   sudo ./nvidia-setup.sh https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_530.30.02_linux.run cuda_12.1.0_530.30.02_linux.run
+# Example Usage (CUDA 12.2)
+#   sudo ./nvidia-setup.sh https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda_12.2.0_535.54.03_linux.run cuda_12.2.0_535.54.03_linux.run
 
 wget $1
 sudo sh $2
