063491158d0f63e13e7c3b7ccf8b9c4be65960a9
edwinlim <edwinlim@node0.a100.datacenter-soc-pg0.wisc.cloudlab.us>
2024-04-01 13:46:50 -0500

    args

--- nvidia-gpu/tensorrt-llm/benchmarking/benchmark_trtllm.py
+++ nvidia-gpu/tensorrt-llm/benchmarking/benchmark_trtllm.py
@@ -8,7 +8,6 @@ from pathlib import Path
 import evaluate
 import numpy as np
 import torch
-from datasets import load_dataset
 from transformers import (AutoModel, AutoModelForCausalLM,
                           AutoModelForSeq2SeqLM, GenerationConfig)
 from utils import DEFAULT_HF_MODEL_DIRS, load_tokenizer, read_model_name
@@ -28,54 +27,59 @@ if PYTHON_BINDINGS:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--hf_model_dir', '--model_dir', type=str, default=None)
     parser.add_argument(
         '--tokenizer_dir',
-        default=None,
-        help='tokenizer path; defaults to hf_model_dir if left unspecified')
-    parser.add_argument('--vocab_file')
-    parser.add_argument('--test_hf', action='store_true')
-    parser.add_argument('--test_trt_llm', action='store_true')
-    parser.add_argument(
-        '--data_type',
         type=str,
-        choices=['fp32', 'fp16', 'bf16', 'float32', 'float16', 'bfloat16'],
-        default='fp16')
-    parser.add_argument('--engine_dir', type=str, default='engine_outputs')
-    parser.add_argument('--use_py_session',
-                        default=False,
-                        action='store_true',
-                        help="Whether or not to use Python runtime session")
+        required=True,
+        help='tokenizer path'
+    )
     parser.add_argument(
-        '--eval_task',
+        '--engine_dir',
         type=str,
-        default='summarize',
-        choices=['summarize', 'summarize_long', 'code_completion'])
-    parser.add_argument('--check_accuracy', action='store_true')
-    parser.add_argument('--tensorrt_llm_rouge1_threshold',
-                        type=float,
-                        default=15.0)
-    parser.add_argument('--eval_ppl', action='store_true')
-    parser.add_argument('--tensorrt_llm_ppl_threshold',
-                        type=float,
-                        default=15.0)
-    parser.add_argument('--dataset_path', type=str, default='')
-    parser.add_argument('--log_level', type=str, default='info')
-    parser.add_argument('--batch_size', type=int, default=1)
-    parser.add_argument('--max_ite', type=int, default=20)
-    parser.add_argument('--output_len', type=int, default=100)
-    parser.add_argument('--max_input_length', type=int, default=923)
+        required=True,
+        help='trtllm engine path'
+    )
+    parser.add_argument(
+        '--dataset_path',
+        type=str,
+        required=True,
+        help='dataset path'
+    )
+    parser.add_argument(
+        '--max_batch_size',
+        type=int,
+        required=True,
+        help='maximum input batch size'
+    )
+    parser.add_argument(
+        '--max_input_tokens',
+        type=int,
+        required=True,
+        help='maximum input prompt length'
+    )
+    parser.add_argument(
+        '--max_output_tokens',
+        type=int,
+        required=True,
+        help='maximum output generation length'
+    )
+    parser.add_argument(
+        '--output_dir',
+        type=str,
+        required=True,
+        help='directory where to save outputs'
+    )
     parser.add_argument(
         '--max_attention_window_size',
         type=int,
         default=None,
-        help=
-        'The attention window size that controls the sliding window attention / cyclic kv cache behavior'
+        help='attention window size for sliding window attention / cyclic kv cache behavior'
     )
-    parser.add_argument('--sink_token_length',
-                        type=int,
-                        default=None,
-                        help='The sink token length.')
+    parser.add_argument(
+        '--sink_token_length',
+        type=int,
+        default=None,
+        help='sink token length.')
     parser.add_argument('--num_beams', type=int, default=1)
     parser.add_argument('--temperature', type=float, default=1.0)
     parser.add_argument('--top_k', type=int, default=1)
@@ -84,41 +88,26 @@ if __name__ == '__main__':
     parser.add_argument('--repetition_penalty', type=float, default=1.0)
     parser.add_argument('--presence_penalty', type=float, default=0.0)
     parser.add_argument('--frequency_penalty', type=float, default=0.0)
-    parser.add_argument('--early_stopping',
-                        type=int,
-                        help='Use early stopping if num_beams > 1'
-                        '1 for early-stopping, 0 for non-early-stopping'
-                        'other values for stopping by length',
-                        default=1)
-    parser.add_argument('--debug_mode',
-                        default=False,
-                        action='store_true',
-                        help="Whether or not to turn on the debug mode")
-    parser.add_argument('--no_add_special_tokens',
-                        dest='add_special_tokens',
-                        default=True,
-                        action='store_false',
-                        help="Whether or not to add special tokens")
     parser.add_argument(
-        '--hf_device_map_auto',
-        action='store_true',
-        help="Use device map 'auto' to load a pretrained HF model. This may "
-        "help to test a large model that cannot fit into a singlue GPU.")
-    parser.add_argument(
-        '--output_dir',
-        type=str,
-        default=None,
-        help="Directory where to save output sentences. 'trtllm.out' for "
-        "TensorRT-LLM outputs, and 'hf.out' for HF outputs.  If None, do not "
-        "save outputs.")
-    parser.add_argument(
-        '--medusa_choices',
-        type=str,
-        default=None,
-        help="Medusa choice to use, if not none, will use Medusa decoding."
-        "   E.g.: [[0, 0, 0, 0], [0, 1, 0], [1, 0], [1, 1]] for 9 medusa tokens."
+        '--early_stopping',
+        type=int,
+        default=1,
+        help='use early stopping if num_beams > 1'
+        '1 for early-stopping, 0 for non-early-stopping'
+        'other values for stopping by length'
     )
-
+    parser.add_argument(
+        '--debug_mode',
+        default=False,
+        action='store_true',
+        help="whether or not to turn on the debug mode"
+    )
+    parser.add_argument(
+        '--add_special_tokens',
+        default=False,
+        action='store_true',
+        help="Whether or not to add special tokens"
+    )
     args = parser.parse_args()
 
-    main(args)
+    #main(args)
