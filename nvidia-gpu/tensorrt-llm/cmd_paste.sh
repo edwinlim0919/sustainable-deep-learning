dd285d8f4acffc92e290890b702f0d10a871bd71
edwinlim <edwinlim@node0.a100.datacenter-soc-pg0.wisc.cloudlab.us>
2024-04-01 18:26:25 -0500

    goodbye, my sweet A100.

--- nvidia-gpu/tensorrt-llm/cmd_paste.sh
+++ nvidia-gpu/tensorrt-llm/cmd_paste.sh
@@ -81,9 +81,12 @@ python ../summarize.py --test_trt_llm --hf_model_dir ./meta-llama/Llama-2-7b-cha
 
 # /dev/shm/sustainable-deep-learning/nvidia-gpu/tensorrt-llm
 sudo docker cp benchmarking/benchmark_trtllm.py 5742c720375f:/app/tensorrt_llm/examples/benchmark_trtllm.py
+sudo docker cp benchmarking/benchmark_utils.py 5742c720375f:/app/tensorrt_llm/examples/benchmark_utils.py
 sudo docker cp ShareGPT_V3_unfiltered_cleaned_split.json 5742c720375f:/app/tensorrt_llm/examples/ShareGPT_V3_unfiltered_cleaned_split.json
+sudo docker cp ShareGPT_V3_unfiltered_cleaned_split_top100.json 5742c720375f:/app/tensorrt_llm/examples/ShareGPT_V3_unfiltered_cleaned_split_top100.json
 
 # /app/tensorrt_llm/examples/llama
 python ../benchmark_trtllm.py --tokenizer_dir ./meta-llama/Llama-2-7b-chat-hf_tokenizer/ --engine_dir ./llama/7B/trt_engines/fp16/1-gpu/ --dataset_path ../ShareGPT_V3_unfiltered_cleaned_split.json --max_batch_size 1 --max_input_tokens 1000 --max_output_tokens 1000 --output_dir ./outputs/llama/7B/fp16/1-gpu 
+python ../benchmark_trtllm.py --tokenizer_dir ./meta-llama/Llama-2-7b-chat-hf_tokenizer/ --engine_dir ./llama/7B/trt_engines/fp16/1-gpu/ --dataset_path ../ShareGPT_V3_unfiltered_cleaned_split_top100.json --num_requests_sample 1 --max_batch_size 1 --max_input_tokens 1000 --max_output_tokens 1000 --output_dir ./outputs/llama/7B/fp16/1-gpu 
 
 #sudo docker cp docker_cp_test.txt 5742c720375f:/app/tensorrt_llm/examples/llama
