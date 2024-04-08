9a3123b9b39da67a53d01346148c4d5473f91796
edwinlim <edwinlim@node0.v10032gb.datacenter-soc-pg0.clemson.cloudlab.us>
2024-04-08 17:10:09 -0400

    cloudlab moment

--- nvidia-gpu/tensorrt-llm/benchmarking/nvsmi_monitor.py
+++ nvidia-gpu/tensorrt-llm/benchmarking/nvsmi_monitor.py
@@ -1,20 +1,43 @@
 import subprocess
 import argparse
 import re
+import time
 
 
 power_usage_pattern = r"\d+W / \d+W"
+memory_usage_pattern = r"\d+MiB / \d+MiB"
+gpu_utilization_pattern = r"\d+%"
+timestamp_pattern = r"(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d"
 
 
 def get_nvsmi_info_V100S_PCIE_32GB():
     output = subprocess.check_output(['nvidia-smi'])
     decoded_output = output.decode('utf-8')
-    for line in decoded_output.split('\n'):
-        #print(line)
-        power_usage_match = re.search(power_usage_pattern, line)
+    nvsmi_dict = {}
 
+    for line in decoded_output.split('\n'):
+        power_usage_match = re.search(power_usage_pattern, line)
+        memory_usage_match = re.search(memory_usage_pattern, line)
+        gpu_utilization_match = re.search(gpu_utilization_pattern, line)
+        timestamp_match = re.search(timestamp_pattern, line)
+
+        if timestamp_match:
+            nvsmi_dict['timestamp'] = timestamp_match.group()
         if power_usage_match:
-            print("Found: ", power_usage_match.group())
+            nvsmi_dict['power_usage'] = power_usage_match.group()
+        if memory_usage_match:
+            nvsmi_dict['memory_usage'] = memory_usage_match.group()
+        if gpu_utilization_match:
+            nvsmi_dict['gpu_utilization'] = gpu_utilization_match.group()
+
+    #print(nvsmi_dict)
+
+    #timestamp = time.time()
+    #local_time = time.localtime(timestamp)
+    #formatted_time = time.strftime("%H:%M:%S", local_time)
+    #print(timestamp)
+    #print(local_time)
+    #print(formatted_time)
     #print(output)
 
 
