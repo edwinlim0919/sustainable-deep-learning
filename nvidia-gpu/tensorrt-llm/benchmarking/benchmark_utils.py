1cfc37af4f5110b7fcfd8ae9e18ef307d10be4e2
edwinlim <edwinlim@node0.v10032gb.datacenter-soc-pg0.clemson.cloudlab.us>
2024-04-10 17:56:55 -0400

    added option for prompt formatting, added batch_dict parsing

--- nvidia-gpu/tensorrt-llm/benchmarking/benchmark_utils.py
+++ nvidia-gpu/tensorrt-llm/benchmarking/benchmark_utils.py
@@ -44,6 +44,7 @@ def prepare_inputs(batch_input_texts: list[str],
     return batch_input_ids
 
 
+# Parses and modifies batch dictionary info into a more storeable format
 def parse_batch_dict(
     batch_dict: dict,
     tokenizer: AutoTokenizer
@@ -67,9 +68,10 @@ def parse_batch_dict(
     batch_dict['device'] = device
     batch_dict['dtype'] = dtype
 
-    for key, value in batch_dict.items():
-        print(f'parse_batch_dict key: {key}, value: {value}')
-    print()
+    #for key, value in batch_dict.items():
+    #    print(f'parse_batch_dict key: {key}, value: {value}')
+    #print()
+    #return batch_dict
 
 
 #def write_results(
@@ -80,12 +82,12 @@ def parse_batch_dict(
 # General Llama2 prompt formatting given a list of message dicts
 # Prompt interleaving should look like: <human> <gpt> <human> <gpt> ...
 # Adapted from code in https://huggingface.co/TheBloke/Llama-2-13B-chat-GPTQ/discussions/5
-# TODO: Prompting like this seems to over-moralize the model...
-def llama2_prompt_general(prompts: list[dict]):
-    B_INST, E_INST = "[INST]", "[/INST]"
-    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-    DEFAULT_SYSTEM_PROMPT = f"""You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
+# TODO: Prompting like this seems to over-moralize the model, but not doing it results in nonsensical output...
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+DEFAULT_SYSTEM_PROMPT = f"""You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
 
+def llama2_prompt_general(prompts: list[dict]):
     if prompts[0]["role"] != "system":
         prompts = [{
             "role": "system",
@@ -107,7 +109,6 @@ def llama2_prompt_general(prompts: list[dict]):
     prompts_list[-1] = prompts_list[-1] + f' {B_INST}'
 
     return "".join(prompts_list)
-
 
 # Sampling dataset prompts for throughput experiments
 def sample_dataset_prompts_no_formatting(
@@ -174,5 +175,104 @@ def sample_dataset_prompts_no_formatting(
     if num_requests_sample < 1:
         num_requests_sample = len(filtered_dataset)
     sampled_prompts = random.sample(filtered_dataset, num_requests_sample)
+
+    return sampled_prompts
+
+def sample_dataset_prompts_with_formatting(
+    dataset_path: str,
+    num_requests_sample: int,
+    max_output_tokens: int,
+    max_input_tokens: int,
+    tokenizer: AutoTokenizer
+):
+    with open(dataset_path, 'r', encoding='utf-8') as f:
+        dataset = json.load(f)
+
+    # Filter out the conversations with less than 2 turns
+    dataset = [data for data in dataset if len(data["conversations"]) >= 2]
+
+    # Only keep conversations that were initiated by a human and responded by GPT
+    human_initiated_dataset = []
+    for data in dataset:
+        if (data['conversations'][0]['from'] == 'human' and
+            data['conversations'][1]['from'] == 'gpt'):
+            human_initiated_dataset.append(data)
+    dataset = human_initiated_dataset
+
+    # Only keep the first two turns of each conversation and use Llama2 dict format
+    llama2_dict_dataset = []
+    for data in dataset:
+        human_dict = {
+            'role': data['conversations'][0]['from'],
+            'content': data['conversations'][0]['value']
+        }
+        gpt_dict = {
+            'role': data['conversations'][1]['from'],
+            'content': data['conversations'][1]['value']
+        }
+        llama2_dict_dataset.append([
+            human_dict,
+            gpt_dict
+        ])
+    dataset = llama2_dict_dataset
+
+    # Format with Llama2 prompt style
+    llama2_format_dataset = []
+    for data in dataset:
+        llama2_conv = llama2_prompt_general(data).split(E_INST)
+        llama2_human = f'{llama2_conv[0]} {E_INST}'
+        llama2_gpt = f'{E_INST} {llama2_conv[1]}'
+        human_dict = {
+            'role': data[0]['role'],
+            'content': llama2_human
+        }
+        gpt_dict = {
+            'role': data[1]['role'],
+            'content': llama2_gpt
+        }
+        llama2_format_dataset.append([
+            human_dict,
+            gpt_dict
+        ])
+    dataset = llama2_format_dataset
+
+    # Tokenize the prompts and completions
+    prompts = [data[0]['content'] for data in dataset]
+    prompt_token_ids = tokenizer(prompts).input_ids
+    completions = [data[1]['content'] for data in dataset]
+    completion_token_ids = tokenizer(completions).input_ids
+
+    # Filter out too long or too short sequences
+    # Real limit is 1020 for prompts, but doing 1000 just to make sure
+    assert(len(dataset) == len(prompts) and
+           len(dataset) == len(completions))
+    filtered_dataset = []
+    for i in range(len(dataset)):
+        num_prompt_tokens = len(prompt_token_ids[i])
+        num_completion_tokens = len(completion_token_ids[i])
+        if num_prompt_tokens < 4 or num_completion_tokens < 4:
+            continue
+        if num_prompt_tokens > max_input_tokens or num_completion_tokens > max_output_tokens:
+            continue
+        filtered_dataset.append(dataset[i])
+    dataset = filtered_dataset
+
+    # Get prompt, prompt tokens, and # output length
+    llama2_prompts = []
+    for data in dataset:
+        llama2_human = data[0]['content']
+        llama2_gpt = data[1]['content']
+        llama2_human_tokens = tokenizer(llama2_human).input_ids
+        llama2_gpt_tokens = tokenizer(llama2_gpt).input_ids
+        llama2_prompts.append((
+            llama2_human,
+            len(llama2_human_tokens),
+            len(llama2_gpt_tokens)
+        ))
+
+    # Sample the prompts
+    if num_requests_sample < 1:
+        num_requests_sample = len(llama2_prompts)
+    sampled_prompts = random.sample(llama2_prompts, num_requests_sample)
 
     return sampled_prompts
