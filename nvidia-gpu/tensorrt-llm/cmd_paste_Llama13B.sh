05ca12f7a44f1cd8d3a1d0281d7d19c1512f3bf2
edwinlim <edwinlim@node0.v10032gb.datacenter-soc-pg0.clemson.cloudlab.us>
2024-04-10 11:37:09 -0400

    README

--- nvidia-gpu/tensorrt-llm/cmd_paste_Llama13B.sh
+++ nvidia-gpu/tensorrt-llm/cmd_paste_Llama13B.sh
@@ -12,6 +12,10 @@ pip install aiofiles
 python convert_checkpoint.py --model_dir meta-llama/Llama-2-13b-chat-hf_model --dtype float16 --output_dir ./llama/13B/trt_ckpt/fp16/1-gpu/ --load_by_shard
 trtllm-build --checkpoint_dir ./llama/13B/trt_ckpt/fp16/1-gpu/ --gemm_plugin float16 --output_dir ./llama/13B/trt_engines/fp16/1-gpu-1-batch/ --max_batch_size 1
 
+# /dev/shm/sustainable-deep-learning/nvidia-gpu/tensorrt-llm
+sudo docker cp benchmarking/benchmark_trtllm.py f88488e6d7ff:/app/tensorrt_llm/examples/benchmark_trtllm.py
+sudo docker cp benchmarking/benchmark_utils.py f88488e6d7ff:/app/tensorrt_llm/examples/benchmark_utils.py
+
 # /app/tensorrt_llm/examples/llama
 # dev testing
 python ../benchmark_trtllm.py --tokenizer_dir ./meta-llama/Llama-2-13b-chat-hf_tokenizer/ --engine_dir ./llama/13B/trt_engines/fp16/1-gpu-1-batch/ --dataset_path ../ShareGPT_V3_unfiltered_cleaned_split_top100.json --num_requests_sample 4 --max_batch_size 1 --max_input_tokens 1000 --max_output_tokens 1000 --output_dir ./outputs/llama/13B/fp16/1-gpu-1-batch --output_file dev_testing.out --random_seed 42 --num_iterations 10
