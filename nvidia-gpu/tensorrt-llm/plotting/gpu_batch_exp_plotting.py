468e292b0294555e6e41663335fa142cf341256b
edwinlim <edwinlim@node0.plotting.datacenter-soc-pg0.clemson.cloudlab.us>
2024-04-16 22:36:27 -0400

    cloudlab moment

--- nvidia-gpu/tensorrt-llm/plotting/gpu_batch_exp_plotting.py
+++ nvidia-gpu/tensorrt-llm/plotting/gpu_batch_exp_plotting.py
@@ -5,57 +5,12 @@ import ast
 from pathlib import Path
 
 
-#max_batch_size_pattern = r"'max_batch_size': (\d+)"
-#iteration_pattern = r"'iteration': (\d+)"
-#max_input_tokens_pattern = r"'max_input_tokens': (\d+)"
-#max_output_tokens_pattern = r"'max_output_tokens': (\d+)"
-#batch_input_lengths_pattern = r"'batch_input_lengths': \[([\d, ]+)\]"
-#
-#def parse_bmark_output(bmark_output_path):
-#    with open(bmark_output_path, 'r') as f:
-#        bmark_output_lines = f.readlines()
-#
-#    eng_path_line = bmark_output_lines[0]
-#    tok_path_line = bmark_output_lines[1]
-#    num_iter_line = bmark_output_lines[2]
-#
-#    current_dict_line = ''
-#    for line in bmark_output_lines[3:]:
-#        stripped = line.strip()
-#        #print(stripped)
-#        if stripped[0] == '{':
-#            current_dict_line = stripped
-#
-#        if stripped[-1] != '}':
-#            current_dict_line += stripped
-#            continue
-#        else:
-#            current_dict_line += stripped
-#            # this is the full result dict
-#
-#            max_batch_size = re.search(max_batch_size_pattern, current_dict_line).group()
-#            iteration = re.search(iteration_pattern, current_dict_line).group()
-#            max_input_tokens = re.search(max_input_tokens_pattern, current_dict_line).group()
-#            max_output_tokens = re.search(max_output_tokens_pattern, current_dict_line).group()
-#            batch_input_lengths = re.search(batch_input_lengths_pattern, current_dict_line).group()
-#
-#            print(f'max_batch_size: {max_batch_size}')
-#            print(f'iteration: {iteration}')
-#            print(f'max_input_tokens: {max_input_tokens}')
-#            print(f'max_output_tokens: {max_output_tokens}')
-#            print(f'batch_input_lengths: {batch_input_lengths}')
-#            print()
-#
-#    print(bmark_output_lines[-1])
-
-
 batch_input_tokens_pattern = r'batch_input_tokens\[(\d+)\]: \[(.*?)\]'
 batch_output_tokens_pattern = r'batch_output_tokens\[(\d+)\]: \[(.*?)\]'
 
 batch_input_lengths_pattern = r'batch_input_lengths\[(\d+)\]'
-#batch_output_lengths_pattern = r'batch_output_lengths\[(\d+)\]'
 batch_output_lengths_pattern = r'batch_output_lengths\[(\d+)\]: \[(.*?)\]'
-
+#batch_output_lengths_pattern = r'batch_output_lengths\[(\d+)\]'
 
 def parse_bmark_output(bmark_output_path):
     with open(bmark_output_path, 'r') as f:
@@ -75,33 +30,42 @@ def parse_bmark_output(bmark_output_path):
             bmark_info[curr_iteration]['batch_input_lengths'] = {}
             bmark_info[curr_iteration]['batch_output_tokens'] = {}
             bmark_info[curr_iteration]['batch_output_lengths'] = {}
+
         if 'batch_size' in line:
             bmark_info[curr_iteration]['batch_size'] = int(line.strip().split()[-1])
+
         if 'max_input_tokens' in line:
             bmark_info[curr_iteration]['max_input_tokens'] = int(line.strip().split()[-1])
+
         if 'max_output_tokens' in line:
             bmark_info[curr_iteration]['max_output_tokens'] = int(line.strip().split()[-1])
+
         if 'batch_start_time' in line:
             bmark_info[curr_iteration]['batch_start_time'] = float(line.strip().split()[-1])
+
         if 'batch_end_time' in line:
             bmark_info[curr_iteration]['batch_end_time'] = float(line.strip().split()[-1])
+
         if 'batch_input_tokens' in line:
             batch_input_tokens_match = re.search(batch_input_tokens_pattern, line)
             batch_input_tokens_index = int(batch_input_tokens_match.group(1))
             token_list_str = batch_input_tokens_match.group(2)
             batch_input_tokens_list = ast.literal_eval(f'[{token_list_str}]')
             bmark_info[curr_iteration]['batch_input_tokens'][batch_input_tokens_index] = batch_input_tokens_list
+
         if 'batch_input_lengths' in line:
             batch_input_lengths_match = re.search(batch_input_lengths_pattern, line)
             batch_input_lengths_index = int(batch_input_lengths_match.group(1))
             batch_input_lengths = int(line.strip().split()[-1])
             bmark_info[curr_iteration]['batch_input_lengths'][batch_input_lengths_index] = batch_input_lengths
+
         if 'batch_output_tokens' in line:
             batch_output_tokens_match = re.search(batch_output_tokens_pattern, line)
             batch_output_tokens_index = int(batch_output_tokens_match.group(1))
             token_list_str = batch_output_tokens_match.group(2)
             batch_output_tokens_list = ast.literal_eval(f'[{token_list_str}]')
             bmark_info[curr_iteration]['batch_output_tokens'][batch_output_tokens_index] = batch_output_tokens_list
+
         if 'batch_output_lengths' in line:
             batch_output_lengths_match = re.search(batch_output_lengths_pattern, line)
             batch_output_lengths_index = int(batch_output_lengths_match.group(1))
@@ -112,22 +76,39 @@ def parse_bmark_output(bmark_output_path):
 
     return bmark_info
 
-    #print(bmark_info[0])
-    #for key, value in bmark_info[99].items():
-    #    print(f'{key}: {value}')
 
-    #for i in range(num_iterations):
-        
+power_usage_pattern = r'(\d+)W / (\d+)W'
+memory_usage_pattern = r'(\d+)MiB / (\d+)MiB'
+gpu_utilization_pattern = r'(\d+)%'
+
 def parse_nvsmi_output(nvsmi_output_path):
     with open(nvsmi_output_path, 'r') as f:
         nvsmi_output_lines = f.readlines()
-
     hardware_platform_line = nvsmi_output_lines[0] # TODO: currently unused
-    print(hardware_platform_line)
 
+    nvsmi_info = []
     for line in nvsmi_output_lines[1:]:
         nvsmi_dict = ast.literal_eval(line)
-        print(nvsmi_dict)
+
+        power_usage_match = re.search(power_usage_pattern, nvsmi_dict['power_usage'])
+        curr_power_usage = int(power_usage_match.group(1))
+        max_power_usage = int(power_usage_match.group(2))
+        nvsmi_dict['curr_power_usage'] = curr_power_usage
+        nvsmi_dict['max_power_usage'] = max_power_usage
+
+        memory_usage_match = re.search(memory_usage_pattern, nvsmi_dict['memory_usage'])
+        curr_memory_usage = int(memory_usage_match.group(1))
+        max_memory_usage = int(memory_usage_match.group(2))
+        nvsmi_dict['curr_memory_usage'] = curr_memory_usage
+        nvsmi_dict['max_memory_usage'] = max_memory_usage
+
+        gpu_utilization_match = re.search(gpu_utilization_pattern, nvsmi_dict['gpu_utilization'])
+        gpu_utilization_percent = int(gpu_utilization_match.group(1))
+        nvsmi_dict['gpu_utilization_percent'] = gpu_utilization_percent
+
+        nvsmi_info.append(nvsmi_dict) # TODO: make sure that times are strictly increasing in order
+
+    return nvsmi_info
 
 
 def main(args):
@@ -137,15 +118,20 @@ def main(args):
     assert(len(bmark_output_paths) == len(nvsmi_output_paths) and 
            len(nvsmi_output_paths) == len(bmark_info))
 
+    bmark_entries = []
     for i in range(len(bmark_output_paths)):
         #bmark_output_path = bmark_output_paths[i]
         #nvsmi_output_path = nvsmi_output_paths[i]
         #print(f'BMARK: {bmark_output_path}')
         #print(f'NVSMI: {nvsmi_output_path}')
+        bmark_entry = {}
         curr_bmark_info = bmark_info[i].split()
         model_size_GB = int(curr_bmark_info[0])
         batch_size = int(curr_bmark_info[1])
         max_sequence_length = int(curr_bmark_info[2])
+
+        bmark_entry['model_size_GB'] = model_size_GB
+        bmark_entry['']
 
     parse_nvsmi_output(nvsmi_output_paths[2])
     #parse_bmark_output(bmark_output_paths[2])
