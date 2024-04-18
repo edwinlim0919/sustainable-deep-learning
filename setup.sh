e34ae57eea84bc9bb3661fb8e8e5b6e0228a1d0d
edwinlim <edwinlim@node0.chris1.datacenter-soc-pg0.clemson.cloudlab.us>
2024-04-18 13:20:08 +0000

    huh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
 sudo apt-get install libssl-dev -y
 sudo apt-get install libz-dev -y
 sudo apt-get install luarocks -y
@@ -16,19 +17,10 @@ bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 
-sudo apt install docker.io
+#sudo apt install docker.io
+sudo apt-get update
+#sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt-get remove containerd.io
+sudo apt install docker.io docker-compose -y
 sudo apt install docker-buildx
 sudo docker buildx install
-#for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-#sudo apt-get update
-#sudo apt-get install ca-certificates curl
-#sudo install -m 0755 -d /etc/apt/keyrings
-#sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-#sudo chmod a+r /etc/apt/keyrings/docker.asc
-#
-#echo \
-#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#sudo apt-get update
-#sudo systemctl start docker
