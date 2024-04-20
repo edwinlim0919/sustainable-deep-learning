e4af60ff867e7e8f41161bd3ccd2e489601de52a
edgeml <edwinlim0919@gmail.com>
2024-04-20 19:04:51 -0400

    jetson setup

---
+++ setup_aarch64.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install libssl-dev -y
+sudo apt-get install libz-dev -y
+sudo apt-get install luarocks -y
+sudo luarocks install luasocket
+sudo apt install linux-tools-`uname -r` linux-tools-generic htop -y
+sudo apt install libelf-dev libdw-dev systemtap-sdt-dev libunwind-dev libslang2-dev libnuma-dev libiberty-dev -y
+
+sudo apt-get update
+#sudo apt-get install -y cmake
+
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+
+#sudo apt install docker.io
+sudo apt-get update
+#sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt-get remove containerd.io
+#sudo apt install docker.io docker-compose -y
+sudo apt-get install docker.io
+sudo apt-get remove docker-buildx-plugin
+sudo apt install docker-buildx
+sudo docker buildx install
