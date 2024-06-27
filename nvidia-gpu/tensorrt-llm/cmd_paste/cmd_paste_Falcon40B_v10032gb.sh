8eda53549bd4fdc8199217c16f2c5825b48057c2
edwinlim <edwinlim@node0.v10032gb.datacenter-soc-pg0.clemson.cloudlab.us>
2024-06-26 22:39:14 -0400

    cmd paste

--- nvidia-gpu/tensorrt-llm/cmd_paste/cmd_paste_Falcon40B_v10032gb.sh
+++ nvidia-gpu/tensorrt-llm/cmd_paste/cmd_paste_Falcon40B_v10032gb.sh
@@ -1,5 +1,15 @@
 # TensorRT-LLM standalone
 sed -i "s/eb316aafb619/b250d4dd5d36/g" cmd_paste_Falcon40B_v10032gb.sh
 
-# Setting up multi-node MPI
-cat /etc/hosts # Both nodes in your experiment should be listed here (e.g. node0, node1, ...)
+
+
+# ---------- SETTING UP MULTI-NODE MPI ----------
+# Find IP address of head node
+hostname -I
+
+# Initialize Docker Swarm on the head node
+docker swarm init --advertise-addr 130.127.134.25
+
+
+#cat /etc/hosts # Both nodes in your experiment should be listed here (e.g. node0, node1, ...)
+docker swarm join --token SWMTKN-1-2gkral686j38dapepynjo7oz08xl2y21uzj7g6nwracyofmqw8-bfqoc8mhtrasa5tak15asiyxn 130.127.134.25:2377
