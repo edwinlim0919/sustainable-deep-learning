200fd9e0b6aadb31c7c7d1dbb8db1e94e7998039
edwinlim <edwinlim@node0.v10032gb.datacenter-soc-pg0.clemson.cloudlab.us>
2024-06-27 14:31:45 -0400

    head/worker designations

--- nvidia-gpu/tensorrt-llm/cmd_paste/cmd_paste_Falcon40B_v10032gb.sh
+++ nvidia-gpu/tensorrt-llm/cmd_paste/cmd_paste_Falcon40B_v10032gb.sh
@@ -4,18 +4,18 @@ sed -i "s/eb316aafb619/b250d4dd5d36/g" cmd_paste_Falcon40B_v10032gb.sh
 
 # ---------- SETTING UP MULTI-NODE MPI ----------
 # /dev/shm/sustainable-deep-learning/nvidia-gpu/tensorrt-llm
-# Find IP address of head node
+# (HEAD) Find IP address of head node
 hostname -I
-# Initialize Docker Swarm on the head node
+# (HEAD) Initialize Docker Swarm on the head node
 sudo docker swarm init --advertise-addr 130.127.134.25
-# Join the Docker Swarm from the worker node(s)
+# (WORKER) Join the Docker Swarm from the worker node(s)
 sudo docker swarm join --token SWMTKN-1-2gkral686j38dapepynjo7oz08xl2y21uzj7g6nwracyofmqw8-bfqoc8mhtrasa5tak15asiyxn 130.127.134.25:2377
-# Create an overlay network on the head node
+# (HEAD) Create an overlay network on the head node
 sudo docker network create --driver overlay --attachable trtllm_network
-# Join the overlay network on both the head node and worker node(s)
+# (HEAD + WORKER) Join the overlay network on both the head node and worker node(s)
 sudo docker network connect trtllm_network great_williamson
 sudo docker network connect trtllm_network dazzling_hofstadter
-# Verify that all containers are connected to the network
+# (HEAD + WORKER) Verify that all containers are connected to the network
 sudo docker network inspect trtllm_network
 
 # /TensorRT-LLM/examples/falcon
@@ -37,13 +37,14 @@ echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClHON3vni5zpJXoxh8jA4BOQI98KvbVstYN9
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 # Test out the MPI connection
-# COPY mpi_test.py TO BOTH DOCKER CONTAINERS IN /TensorRT-LLM/examples/falcon/mpi_test.py
+# (HEAD + WORKER) COPY mpi_test.py TO BOTH DOCKER CONTAINERS IN /TensorRT-LLM/examples/falcon/mpi_test.py
 sudo docker cp benchmarking/mpi_test.py b250d4dd5d36:/TensorRT-LLM/examples/falcon/mpi_test.py
 mpirun -n 4 --hostfile hostfile --allow-run-as-root --oversubscribe python3 mpi_test.py
 
 
 # ---------- SETTING UP FALCON 40B ----------
 # /TensorRT-LLM/examples/falcon
+# (HEAD + WORKER) Install and download some prerequisites for Falcon 40B
 pip install -r requirements.txt
 apt-get update
 apt-get -y install git git-lfs
@@ -51,7 +52,7 @@ git lfs install
 git clone https://huggingface.co/tiiuae/falcon-40b-instruct falcon/40b-instruct
 
 # /TensorRT-LLM/examples/falcon
-# 4-way tensor parallelism + 1-way pipeline parallelism
+# (HEAD + WORKER) 4-way tensor parallelism + 1-way pipeline parallelism
 python3 convert_checkpoint.py --model_dir ./falcon/40b-instruct --dtype float16 --output_dir ./falcon/40b-instruct/trt_ckpt/fp16/tp4-pp1/ --tp_size 4 --pp_size 1 --load_by_shard
 trtllm-build --checkpoint_dir ./falcon/40b-instruct/trt_ckpt/fp16/tp4-pp1/ --gemm_plugin float16 --gpt_attention_plugin float16 --output_dir ./falcon/40b-instruct/trt_engines/fp16/tp4-pp1-batch1/
 mpirun -n 4 --allow-run-as-root --oversubscribe python3 ../summarize.py --test_trt_llm --hf_model_dir ./falcon/40b-instruct/ --engine_dir ./falcon/40b-instruct/trt_engines/fp16/tp4-pp1-batch1/
